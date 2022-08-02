a92d5821787b1e2c39e7e037c3613bc1c92fbe12
Dario Alejandro <91350692+darioah@users.noreply.github.com>
2022-08-02 15:29:12 -0300

    Update app2.js

--- Dario_ Cardozo_Modulo2_Tarea4/script/app2.js
+++ Dario_ Cardozo_Modulo2_Tarea4/script/app2.js
@@ -1,20 +1,4 @@
-// MODO DARK AQUI 
-const prereferdColorScheme = window.matchMedia('prefers-color-scheme: dark').matches ? 'dark' : 'light';
-const slider = document.getElementById('slider')
-const setTheme = (theme) => {
-document.documentElement.setAttribute('date-theme', theme)
-localStorage.setItem('theme', theme)
-}
-console.log(setTheme)
 
-slider.addEventListener('click', () => {
-let switckToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark'
-setTheme(switckToTheme)
-console.log(switckToTheme)
-});
-setTheme(localStorage.getItem('theme'|| prereferdColorScheme));
-// FIN DEL MODO DARK
- 
 
 
 const { createApp } = Vue
@@ -106,7 +90,23 @@ createApp({
           let divisor = this.total_ID > 0? 3 : 2   
           this.promedioTotal = Number(((this.total_D+this.total_R + this.total_ID)/divisor).toFixed(2))
           
-            
+            // MODO DARK AQUI 
+const prereferdColorScheme = window.matchMedia('prefers-color-scheme: dark').matches ? 'dark' : 'light';
+const slider = document.getElementById('slider')
+const setTheme = (theme) => {
+document.documentElement.setAttribute('date-theme', theme)
+localStorage.setItem('theme', theme)
+}
+console.log(setTheme)
+
+slider.addEventListener('click', () => {
+let switckToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark'
+setTheme(switckToTheme)
+console.log(switckToTheme)
+});
+setTheme(localStorage.getItem('theme'|| prereferdColorScheme));
+// FIN DEL MODO DARK
+ 
       }  )
       .catch(error=> console.log(error))
   },
@@ -172,84 +172,4 @@ computed:{
 
 
 
-  
 
-
-
-
-
-
-//listMemberS = Array.from(senate.results[0].members);  
-//listMembersH = Array.from(house.results[0].members); 
-
-// if( document.title == "Attendance-House" ){
-  
- 
-  
-  //this.listMembersPCT = Math.ceil(this.list.listMembersH.length * 0.1);
-  
-  
-   // app.leastEngaged = orderVotes(listMembersH, "TOP", "missed_votes_pct");
-    //app.mostEngaged = orderVotes(listMembersH, "BOTON", "missed_votes_pct");
-
-  //   let contenedor = document.getElementById("tabla");
-  // let contenedor2 = document.getElementById("tabla_most");
-  //   mostrarTabla(this.leastEngaged,contenedor, 'missed_votes','missed_votes_pct' );
-  //   mostrarTabla( this.mostEngaged,contenedor2,'missed_votes', 'missed_votes_pct' );
-   
-  // }
-  // else if( document.title === "PartyLoyalty-House"){
-  //   this.listMembersPCT = Math.ceil(this.listMembersH.length * 0.1);
-  //   this.leastLoyalty = orderVotes(this.listMembersH, "BOTON", "votes_with_party_pct");
-  //   this.mostLoyalty = orderVotes(this.listMembersH, "TOP", "votes_with_party_pct");
-  //   filterParty(this.listMembersH); 
-  //   let contenedor = document.getElementById("tabla");
-  //   let contenedor2 = document.getElementById("tabla_most");
-  //   mostrarTabla( this.leastLoyalty,contenedor, 'total_votes','votes_with_party_pct' );
-  //   mostrarTabla( this.mostLoyalty,contenedor2,'total_votes', 'votes_with_party_pct' );
-  // }
-  // else if( document.title === "Attendance-Senate" ){
-  //   this.listMembersPCT = Math.ceil(this.listMemberS.length * 0.1);
-  //   filterParty(this.listMemberS); 
-  //   this.leastEngaged = orderVotes(this.listMemberS, "TOP", "missed_votes_pct");
-  //   this.mostEngaged = orderVotesthis(this.listMemberS, "BOTON", "missed_votes_pct");
-  //   let contenedor = document.getElementById("tabla");
-  //   let contenedor2 = document.getElementById("tabla_most");
-  //   mostrarTabla( this.leastEngaged,contenedor, 'missed_votes','missed_votes_pct' );
-  //   mostrarTabla( this.mostEngaged,contenedor2,'missed_votes', 'missed_votes_pct' );
-  // }
-//   else if( document.title === "PartyLoyalty-Senate"){
-//     this.listMembersPCT = Math.ceil(this.listMemberS.length * 0.1);
-//     filterParty(this.listMemberS); 
-//     this.leastLoyalty = orderVotes(this.listMemberS, "BOTON", "votes_with_party_pct");
-//     this.mostLoyalty = orderVotesthis(this.listMemberS, "TOP", "votes_with_party_pct");
-//   let contenedor = document.getElementById("tabla");
-//   let contenedor2 = document.getElementById("tabla_most");
-//   mostrarTabla( this.leastLoyalty,contenedor, 'total_votes','votes_with_party_pct' );
-//   mostrarTabla( this.mostLoyalty,contenedor2,'total_votes', 'votes_with_party_pct' );
-//   } 
-// mostrarPorcentaje(this.allGlance.democrats);
-// mostrarPorcentaje(this.allGlance.republicans);
-// mostrarPorcentaje(this.allGlance.independents);
-
-
-// this.total_D = (this.avorege.democrats/this.allGlance.democrats.length.toFixed(2)) ;
-// this.total_R = (Number(this.avorege.republicans) / this.allGlance.republicans.length).toFixed(2)
-// this.total_ID
-//    if (this.allGlance.independents.length === 0 || !0 ) {    
-//     (this.total_ID = this.avorege.independents).toFixed(2)
-//    }
-//   document.getElementById("prom_D").innerHTML = `${total_D.toFixed(2)}% `;
-//   document.getElementById("prom_R").innerHTML = `${total_R.toFixed(2)}%`;
-//   document.getElementById("prom_ID").innerHTML = `${total_ID.toFixed(2)}%`;
-
-// let dd = this.allGlance.democrats.length;
-// document.getElementById("cantidad").innerHTML = dd;
-// let rr = this.allGlance.republicans.length;
-// document.getElementById("cantidad_R").innerHTML = rr;
-// let id = this.allGlance.independents.length;
-// document.getElementById("cantidad_ID").innerHTML = id;
-// let total = this.allGlance.democrats.length + app.allGlance.republicans.length +app.allGlance.independents.length
-// document.getElementById("total").innerHTML = total;
-// let t_pct = (total_D + total_R + total_ID)/3.
-// document.getElementById("total_pct").innerHTML = `${t_pct.toFixed(2)}%`;
